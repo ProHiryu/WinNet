da5263ea3b3b31f222fb8ec3e1aad5da31036c29
ProHiryu <songheqi1996@gmail.com>
2019-01-07 14:09:32 +0800

    rnn_version_pytorch

---
+++ winnet_rnn.py
@@ -0,0 +1,302 @@
+#!/anaconda3/bin/python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 - songheqi <songheqi1996@gmail.com>
+
+import pandas as pd
+import numpy as np
+import random
+
+df = pd.read_csv('/Users/heqi.song/work/Winnet/data/data.csv')
+df.head()
+
+x = [0,12,13,14,15]
+df_2 = df.drop(df.columns[x], axis=1, inplace=False)
+
+data = []
+
+for _, row in df_2.iterrows():
+    if row[10] in ['True', 'False']:
+        data.append(row)
+
+data = pd.DataFrame(data)
+
+data.to_csv('/Users/heqi.song/work/Winnet/data/clean_data.csv', index=False, header=False)
+labels_df = data[data.columns[10]]
+data_df = data[data.columns[:10]]
+
+words = [word for _, row in data_df[data_df.columns[:10]].iterrows() for word in row]
+
+# feel free to use this import 
+from collections import Counter
+
+## Build a dictionary that maps words to integers
+counts = Counter(words)
+vocab = sorted(counts, key=counts.get, reverse=True)
+vocab_to_int = {word: ii for ii, word in enumerate(vocab, 1)}
+
+## use the dict to tokenize each review in reviews_split
+## store the tokenized reviews in reviews_ints
+reviews_ints = []
+for _, review in data_df.iterrows():
+    reviews_ints.append([vocab_to_int[word] for word in review])
+
+# print(vocab_to_int)
+# print(reviews_ints)
+
+encoded_labels = np.array([1 if label == 'True' else 0 for label in labels_df])
+
+features = np.array(reviews_ints)
+assert(len(features) == len(encoded_labels))
+
+split_frac = 0.8
+
+## split data into training, validation, and test data (features and labels, x and y)
+
+split_idx = int(len(features)*0.8)
+train_x, remaining_x = features[:split_idx], features[split_idx:]
+train_y, remaining_y = encoded_labels[:split_idx], encoded_labels[split_idx:]
+
+test_idx = int(len(remaining_x)*0.5)
+val_x, test_x = remaining_x[:test_idx], remaining_x[test_idx:]
+val_y, test_y = remaining_y[:test_idx], remaining_y[test_idx:]
+
+## print out the shapes of your resultant feature data
+print("\t\t\tFeature Shapes:")
+print("Train set: \t\t{}".format(train_x.shape), 
+      "\nValidation set: \t{}".format(val_x.shape),
+      "\nTest set: \t\t{}".format(test_x.shape))
+
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+
+# create Tensor datasets
+train_data = TensorDataset(torch.from_numpy(train_x), torch.from_numpy(train_y))
+valid_data = TensorDataset(torch.from_numpy(val_x), torch.from_numpy(val_y))
+test_data = TensorDataset(torch.from_numpy(test_x), torch.from_numpy(test_y))
+
+# dataloaders
+batch_size = 20
+
+# make sure the SHUFFLE your training data
+train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size, drop_last=True)
+valid_loader = DataLoader(valid_data, shuffle=True, batch_size=batch_size, drop_last=True)
+test_loader = DataLoader(test_data, shuffle=True, batch_size=batch_size, drop_last=True)
+
+
+# obtain one batch of training data
+dataiter = iter(train_loader)
+sample_x, sample_y = dataiter.next()
+
+print('Sample input size: ', sample_x.size()) # batch_size, seq_length
+print('Sample input: \n', sample_x)
+print()
+print('Sample label size: ', sample_y.size()) # batch_size
+print('Sample label: \n', sample_y)
+
+
+# First checking if GPU is available
+train_on_gpu=torch.cuda.is_available()
+
+if(train_on_gpu):
+    print('Training on GPU.')
+else:
+    print('No GPU available, training on CPU.')
+
+
+import torch.nn as nn
+
+class SentimentRNN(nn.Module):
+    """
+    The RNN model that will be used to perform Sentiment analysis.
+    """
+
+    def __init__(self, vocab_size, output_size, embedding_dim, hidden_dim, n_layers, drop_prob=0.5):
+        """
+        Initialize the model by setting up the layers.
+        """
+        super(SentimentRNN, self).__init__()
+
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.hidden_dim = hidden_dim
+        
+        # embedding and LSTM layers
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, 
+                            dropout=drop_prob, batch_first=True)
+        
+        # dropout layer
+        self.dropout = nn.Dropout(0.3)
+        
+        # linear and sigmoid layers
+        self.fc = nn.Linear(hidden_dim, output_size)
+        self.sig = nn.Sigmoid()
+        
+
+    def forward(self, x, hidden):
+        """
+        Perform a forward pass of our model on some input and hidden state.
+        """
+        batch_size = x.size(0)
+
+        # embeddings and lstm_out
+        embeds = self.embedding(x)
+        lstm_out, hidden = self.lstm(embeds, hidden)
+    
+        # stack up lstm outputs
+        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
+        
+        # dropout and fully-connected layer
+        out = self.dropout(lstm_out)
+        out = self.fc(out)
+        # sigmoid function
+        sig_out = self.sig(out)
+        
+        # reshape to be batch_size first
+        sig_out = sig_out.view(batch_size, -1)
+        sig_out = sig_out[:, -1] # get last batch of labels
+        
+        # return last sigmoid output and hidden state
+        return sig_out, hidden
+    
+    
+    def init_hidden(self, batch_size):
+        ''' Initializes hidden state '''
+        # Create two new tensors with sizes n_layers x batch_size x hidden_dim,
+        # initialized to zero, for hidden state and cell state of LSTM
+        weight = next(self.parameters()).data
+        
+        if (train_on_gpu):
+            hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda(),
+                  weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda())
+        else:
+            hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
+                      weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
+        
+        return hidden
+
+# Instantiate the model w/ hyperparams
+vocab_size = len(vocab_to_int)+1 # +1 for the 0 padding + our word tokens
+output_size = 1
+embedding_dim = 150
+hidden_dim = 256
+n_layers = 4
+
+net = SentimentRNN(vocab_size, output_size, embedding_dim, hidden_dim, n_layers)
+
+print(net)
+
+# loss and optimization functions
+lr=0.001
+
+criterion = nn.BCELoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+
+# training params
+
+epochs = 10 # 3-4 is approx where I noticed the validation loss stop decreasing
+
+counter = 0
+print_every = 100
+clip=5 # gradient clipping
+
+# move model to GPU, if available
+if(train_on_gpu):
+    net.cuda()
+
+net.train()
+# train for some number of epochs
+for e in range(epochs):
+    # initialize hidden state
+    h = net.init_hidden(batch_size)
+
+    # batch loop
+    for inputs, labels in train_loader:
+        counter += 1
+
+        if(train_on_gpu):
+            inputs, labels = inputs.cuda(), labels.cuda()
+
+        # Creating new variables for the hidden state, otherwise
+        # we'd backprop through the entire training history
+        h = tuple([each.data for each in h])
+
+        # zero accumulated gradients
+        net.zero_grad()
+
+        # get the output from the model
+        output, h = net(inputs, h)
+
+        # calculate the loss and perform backprop
+        loss = criterion(output.squeeze(), labels.float())
+        loss.backward()
+        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+        nn.utils.clip_grad_norm_(net.parameters(), clip)
+        optimizer.step()
+
+        # loss stats
+        if counter % print_every == 0:
+            # Get validation loss
+            val_h = net.init_hidden(batch_size)
+            val_losses = []
+            net.eval()
+            for inputs, labels in valid_loader:
+
+                # Creating new variables for the hidden state, otherwise
+                # we'd backprop through the entire training history
+                val_h = tuple([each.data for each in val_h])
+
+                if(train_on_gpu):
+                    inputs, labels = inputs.cuda(), labels.cuda()
+
+                output, val_h = net(inputs, val_h)
+                val_loss = criterion(output.squeeze(), labels.float())
+
+                val_losses.append(val_loss.item())
+
+            net.train()
+            print("Epoch: {}/{}...".format(e+1, epochs),
+                  "Step: {}...".format(counter),
+                  "Loss: {:.6f}...".format(loss.item()),
+                  "Val Loss: {:.6f}".format(np.mean(val_losses)))
+
+test_losses = [] # track loss
+num_correct = 0
+
+# init hidden state
+h = net.init_hidden(batch_size)
+
+net.eval()
+# iterate over test data
+for inputs, labels in test_loader:
+
+    # Creating new variables for the hidden state, otherwise
+    # we'd backprop through the entire training history
+    h = tuple([each.data for each in h])
+
+    if(train_on_gpu):
+        inputs, labels = inputs.cuda(), labels.cuda()
+    
+    # get predicted outputs
+    output, h = net(inputs, h)
+    
+    # calculate loss
+    test_loss = criterion(output.squeeze(), labels.float())
+    test_losses.append(test_loss.item())
+    
+    # convert output probabilities to predicted class (0 or 1)
+    pred = torch.round(output.squeeze())  # rounds to the nearest integer
+    
+    # compare predictions to true label
+    correct_tensor = pred.eq(labels.float().view_as(pred))
+    correct = np.squeeze(correct_tensor.numpy()) if not train_on_gpu else np.squeeze(correct_tensor.cpu().numpy())
+    num_correct += np.sum(correct)
+
+
+# -- stats! -- ##
+# avg test loss
+print("Test loss: {:.3f}".format(np.mean(test_losses)))
+
+# accuracy over all test data
+test_acc = num_correct/len(test_loader.dataset)
+print("Test accuracy: {:.3f}".format(test_acc))
